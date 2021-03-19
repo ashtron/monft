fc5520a32ac94a1340853f6f00a131ddb3fec56a
Ashton Playsted <ashton.playsted@protonmail.com>
2021-03-19 19:24:33 +0200

    Implement minting

--- contracts/MonFT.sol
+++ contracts/MonFT.sol
@@ -3,7 +3,22 @@
 pragma solidity ^0.8.0;
 
 import "./openzeppelin-contracts/token/ERC721/ERC721.sol";
+import "./openzeppelin-contracts/utils/Counters.sol";
 
-contract MonFT {
-    constructor() {}
+// https://docs.openzeppelin.com/contracts/4.x-rc/erc721
+
+contract MonFT is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("MonFT", "MFT") public {}
+
+    function mintMon() public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newMonId = _tokenIds.current();
+
+        _mint(msg.sender, newMonId);
+
+        return newMonId;
+    }
 }

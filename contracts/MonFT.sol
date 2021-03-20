176dd01dfc394741e07c3a1145c35715d9a770a2
Ashton Playsted <ashton.playsted@protonmail.com>
2021-03-20 12:12:19 +0200

    Use a struct to represent DNA sequences instead of a uint

--- contracts/MonFT.sol
+++ contracts/MonFT.sol
@@ -10,39 +10,84 @@ import "./openzeppelin-contracts/utils/Counters.sol";
 contract MonFT is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    uint256 public dnaLength;
 
-    // tokenID => random number
-    mapping(uint256 => uint256) public geneSequences;
+    struct dna {
+        uint256 arms;
+        uint256 body;
+        uint256 ears;
+        uint256 eyes;
+        uint256 legs;
+        uint256 nose;
+        uint256 mouth;
+        uint256 wings;
+    }
 
-    constructor() ERC721("MonFT", "MFT") {}
+    // tokenId => 
+    mapping(uint256 => dna) public dnaSequences; //
+
+    constructor(uint256 _dnaLength) ERC721("MonFT", "MFT") {
+        dnaLength = _dnaLength;
+    }
+
+    // Gene data getter split into two functions to avoid stack too deep error.
+    function getFaceData(uint256 tokenId) public view returns (uint256, uint256, uint256, uint256) {
+        return (
+            dnaSequences[tokenId].ears,
+            dnaSequences[tokenId].eyes,
+            dnaSequences[tokenId].nose,
+            dnaSequences[tokenId].mouth
+        );
+    }
+
+    function getBodyData(uint256 tokenId) public view returns (uint256, uint256, uint256, uint256) {
+        return (
+            dnaSequences[tokenId].arms,
+            dnaSequences[tokenId].body,
+            dnaSequences[tokenId].legs,
+            dnaSequences[tokenId].wings
+        );
+    }
 
     function mintMon() public returns (uint256) {
         _tokenIds.increment();
+
         uint256 newMonId = _tokenIds.current();
-        
-        geneSequences[newMonId] = generateGeneSequence();
+
+        dnaSequences[newMonId] = dna({
+            arms: 1,
+            body: 2,
+            ears: 3,
+            eyes: 4,
+            legs: 5,
+            nose: 6,
+            mouth: 7,
+            wings: 8
+        });
+
         _mint(msg.sender, newMonId);
 
         return newMonId;
     }
 
-    function generateGeneSequence() public view returns (uint256) {
-        uint256 geneSequence = generateRandomNumber() % 10 ** 4;
-        return geneSequence;
-    }
+    // function generatedna() public view returns (uint256) {
+    //     // Generate a random five-digit number.
+    //     uint256 dna = generateRandomNumber() % 10 ** dnaLength;
+    //     return dna;
+    // }
 
-    function generateRandomNumber() public view returns (uint256) {
-        // Pseudorandom for now.
-        return uint(keccak256(abi.encodePacked(block.number)));
-    }
+    // function generateRandomNumber() public view returns (uint256) {
+    //     // Pseudorandom for now.
+    //     return uint(keccak256(abi.encodePacked(block.number)));
+    // }
 
-    function mutate(uint256 tokenId) public {
-        uint256 newGeneSequence = generateGeneSequence();
-        geneSequences[tokenId] = newGeneSequence;
-    }
+    // function mutate(uint256 tokenId) public {
+    //     uint256 newdna = generatedna();
+    //     dnaSequences[tokenId] = newdna;
+    // }
 
-    function transferMon(address from, address to, uint256 tokenId) public {
-        mutate(tokenId);
-        safeTransferFrom(from, to, tokenId);
-    }
+    // function transferMon(address from, address to, uint256 tokenId) public {
+    //     mutate(tokenId);
+    //     safeTransferFrom(from, to, tokenId);
+    // }
 }
--- test/MonFT.js
+++ test/MonFT.js
@@ -11,7 +11,7 @@ describe("MonFT contract", function () {
     MonFT = await ethers.getContractFactory("MonFT");
     [addr1, addr2, ...addrs] = await ethers.getSigners();
 
-    monFT = await MonFT.deploy();
+    monFT = await MonFT.deploy(5);
     await monFT.deployed();
   });
 
@@ -19,36 +19,48 @@ describe("MonFT contract", function () {
     it("Should mint NFTs", async function () {
       await monFT.mintMon();
 
-      expect(await monFT.balanceOf(addr1.address)).to.equal(1);
+      console.log(await monFT.getFaceData(1), await monFT.getBodyData(1));
+
+      // expect(await monFT.balanceOf(addr1.address)).to.equal(1);
     });
 
-    it("Should give NFTs a gene sequence", async function () {
-        await monFT.mintMon();
-        const geneSequence = Number(await monFT.geneSequences(1));
+    // it("Should give NFTs a gene sequence", async function () {
+    //     await monFT.mintMon();
+    //     const geneSequence = Number(await monFT.geneSequences(1));
 
-        // Expect a five-digit gene sequence.
-        expect(String(geneSequence).length === 5);
-      });
+    //     // Expect a five-digit gene sequence.
+    //     expect(String(geneSequence).length).to.eq(5);
+    // });
   });
 
-  describe("transferMon", function () {
-    it("Should transfer NFTs", async function () {
-      await monFT.mintMon();
-      await monFT.transferMon(addr1.address, addr2.address, 1);
+  // describe("transferMon", function () {
+  //   it("Should transfer NFTs", async function () {
+  //     await monFT.mintMon();
+  //     await monFT.transferMon(addr1.address, addr2.address, 1);
 
-      expect(await monFT.ownerOf(1)).to.equal(addr2.address);
-    });
-  });
+  //     expect(await monFT.ownerOf(1)).to.equal(addr2.address);
+  //   });
+  // });
 
-  describe("mutate", function () {
-    it("Should change gene sequence on transfer", async function () {
-      await monFT.mintMon();
-      const oldGeneSequence = await monFT.geneSequences(1);
+  // describe("mutate", function () {
+  //   it("Should change gene sequence on transfer", async function () {
+  //     await monFT.mintMon();
+  //     const oldGeneSequence = await monFT.geneSequences(1);
 
-      await monFT.transferMon(addr1.address, addr2.address, 1);
-      const newGeneSequence = await monFT.geneSequences(1);
+  //     await monFT.transferMon(addr1.address, addr2.address, 1);
+  //     const newGeneSequence = await monFT.geneSequences(1);
       
-      expect(oldGeneSequence).not.to.equal(newGeneSequence);
-    });
-  });
+  //     expect(oldGeneSequence).not.to.equal(newGeneSequence);
+  //   });
+  // });
+
+  // describe("test new gene sequence data structure", function () {
+  //   it("let's see", async function () {
+  //     // const result = Number(await monFT.geneSequences2(1)("eyes"));
+  //     // console.log(result);
+  //     const result = await monFT.getSequenceData(1);
+  //     console.log(Number(result[0]), Number(result[1]), Number(result[2]));
+  //     // expect(result).to.equal(21);
+  //   });
+  // });
 });

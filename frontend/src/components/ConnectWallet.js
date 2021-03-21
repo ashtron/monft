8f48d6f576593dc269db30f3f17321f88b2ee020
Ashton Playsted <ashton.playsted@protonmail.com>
2021-03-21 13:34:20 +0200

    Add Cole's ConnectWallet component update

--- frontend/src/components/ConnectWallet.js
+++ frontend/src/components/ConnectWallet.js
@@ -4,26 +4,33 @@ import { NetworkErrorMessage } from "./NetworkErrorMessage";
 
 export function ConnectWallet({ connectWallet, networkError, dismiss }) {
   return (
-    <div className="container">
-      <div className="row justify-content-md-center">
-        <div className="col-12 text-center">
-          {/* Metamask network should be set to Localhost:8545. */}
-          {networkError && (
-            <NetworkErrorMessage 
-              message={networkError} 
-              dismiss={dismiss} 
-            />
-          )}
-        </div>
-        <div className="col-6 p-4 text-center">
-          <p>Please connect to your wallet.</p>
-          <button
-            className="btn btn-warning"
-            type="button"
-            onClick={connectWallet}
-          >
-            Connect Wallet
-          </button>
+    <div className="vh-100" style={{backgroundColor: "#00ccff"}}>
+      <div className="container">
+        <div className="row justify-content-md-center">
+          <div className="col-12 text-center">
+            {/* Metamask network should be set to Localhost:8545. */}
+            {networkError && (
+              <NetworkErrorMessage 
+                message={networkError} 
+                dismiss={dismiss} 
+              />
+            )}
+          </div>
+          <div className="col-6 p-4 text-light text-center">
+
+            <p class="h3" >Connect your wallet to create or evolve your monster.</p>
+            <div style={{backgroundColor: "#00ccff"}} className="h-75 p-4 col-12">
+          <img className="w-50 m-2" src={require(`../assets/mon_egg_jiggle.gif`)} ></img>
+          </div>
+            <button
+            style={{backgroundColor: "#00ff00"}}
+              className="btn text-light"
+              type="button"
+              onClick={connectWallet}
+            >
+              Connect Wallet
+            </button>
+          </div>
         </div>
       </div>
     </div>

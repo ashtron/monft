364952825c8784160cfbf95ccc52a94568c57138
Ashton Playsted <ashton.playsted@protonmail.com>
2021-03-21 10:07:01 +0200

    Make sure NFT has all parts before mutating individual parts

--- contracts/MonFT.sol
+++ contracts/MonFT.sol
@@ -41,30 +41,59 @@ contract MonFT is ERC721Enumerable {
         return newMonId;
     }
 
+    // Need to think of a more elegant method.
     function mutate(uint256 tokenId) public {
-        // Choose the part to mutate.
-        uint256 partIndex = generateRandomNumber(8);
+        dna storage dnaSequence = dnaSequences[tokenId];
 
         // Choose the new part.
         uint256 newPartIndex = generateRandomNumber(numOptions);
 
-        // Need to think of a more elegant method.
+        // If a part is missing, add it.
+        if (dnaSequence.arms == 0) {
+            dnaSequences[tokenId].arms = newPartIndex;
+            return;
+        } else if (dnaSequence.body == 0) {
+            dnaSequences[tokenId].body = newPartIndex;
+            return;
+        } else if (dnaSequence.legs == 0) {
+            dnaSequences[tokenId].legs = newPartIndex;
+            return;
+        } else if (dnaSequence.wings == 0) {
+            dnaSequences[tokenId].wings = newPartIndex;
+            return;
+        } else if (dnaSequence.ears == 0) {
+            dnaSequences[tokenId].ears = newPartIndex;
+            return;
+        } else if (dnaSequence.eyes == 0) {
+            dnaSequences[tokenId].eyes = newPartIndex;
+            return;
+        } else if (dnaSequence.nose == 0) {
+            dnaSequences[tokenId].nose = newPartIndex;
+            return;
+        } else if (dnaSequence.mouth == 0) {
+            dnaSequences[tokenId].mouth = newPartIndex;
+            return;
+        }
+
+        // If no parts are missing, choose the part to mutate.
+        uint256 partIndex = generateRandomNumber(8);
+
         if (partIndex == 1) {
-            dnaSequences[tokenId].arms = getIndex(dnaSequences[tokenId].arms, newPartIndex);
+            dnaSequence.arms = getIndex(dnaSequence.arms, newPartIndex);
         } else if (partIndex == 2) {
-            dnaSequences[tokenId].body = getIndex(dnaSequences[tokenId].body, newPartIndex);
+            dnaSequence.body = getIndex(dnaSequence.body, newPartIndex);
         } else if (partIndex == 3) {
-            dnaSequences[tokenId].legs = getIndex(dnaSequences[tokenId].legs, newPartIndex);
+            dnaSequence.legs = getIndex(dnaSequence.legs, newPartIndex);
         } else if (partIndex == 4) {
-            dnaSequences[tokenId].wings = getIndex(dnaSequences[tokenId].wings, newPartIndex);
+            dnaSequence.wings = getIndex(dnaSequence.wings, newPartIndex);
         } else if (partIndex == 5) {
-            dnaSequences[tokenId].ears = getIndex(dnaSequences[tokenId].ears, newPartIndex);
+            dnaSequence.ears = getIndex(dnaSequence.ears, newPartIndex);
         } else if (partIndex == 6) {
-            dnaSequences[tokenId].eyes = getIndex(dnaSequences[tokenId].eyes, newPartIndex);
+            dnaSequence.eyes = getIndex(dnaSequence.eyes, newPartIndex);
         } else if (partIndex == 7) {
-            dnaSequences[tokenId].nose = getIndex(dnaSequences[tokenId].nose, newPartIndex);
+            dnaSequence.nose = getIndex(dnaSequence.nose, newPartIndex);
         } else if (partIndex == 8) {
-            dnaSequences[tokenId].mouth = getIndex(dnaSequences[tokenId].mouth, newPartIndex);
+            dnaSequence.mouth = getIndex(dnaSequence.mouth, newPartIndex);
         }
     }
 
